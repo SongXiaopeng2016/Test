185166b1ec14fbca6368210e198d9e859e9f0539
songxiaopeng2016 <646532446@qq.com>
2016-08-01 16:21:17 +0800

    第二次提交

--- app/src/main/java/com/sxp/textapp/MainActivity.java
+++ app/src/main/java/com/sxp/textapp/MainActivity.java
@@ -2,8 +2,13 @@ package com.sxp.textapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
